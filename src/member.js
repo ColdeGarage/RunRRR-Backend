4fa1b31d3bdd3d2a5a14c8794568e817628b7325
qmo1222 <howard19192828@gmail.com>
2017-03-07 22:47:56 +0800

    add uncompleted member's function

---
+++ src/member.js
@@ -0,0 +1,131 @@
+//return login verification
+exports.login = function(req, res){
+	var email = req.body.email;
+	var password = req.body.password;
+
+	var ret = new Object;
+	ret.object = "member";
+	ret.action = "login";
+
+	query("SELECT * FROM member WHERE email = "+email, function(err, rows){
+		if (err){
+			ret.brea = 0;
+			console.log('db error');
+		}
+		else {
+			ret.brea = 1;
+
+			var info = JSON.parse(rows);
+			ret.uid = info['uid'];
+
+			if (password == info['password']) {
+				ret.correct = 1;
+				res.json(ret);
+				console.log('check successfully');
+			}
+			else {
+				ret.correct = 0;
+				res.json(ret);
+				console.log('login error');
+			}
+		}
+	})
+}
+
+//return member's status
+exports.liveordie = function(req, res){
+	var uid = req.body.uid;
+
+	var ret = new Object;
+	ret.uid = uid;
+	ret.object = "member";
+	ret.action = "liveordie";
+
+	db.query("SELECT status FROM member WHERE uid = "+uid, function(err, rows){
+		if (err){
+			ret.brea = false;
+			console.log('db error');
+		}
+		else {
+			ret.brea = true;
+			var info = JSON.parse(rows);
+			ret.status = info['status'];
+			res.json(ret);
+			console.log('send successfully');
+		}
+	})
+}
+
+//update member's location
+exports.update = function(req, res){
+	var member = req.body;
+
+	var ret = new Object;
+	ret.uid = uid;
+	ret.object = "member";
+	ret.action = "update";
+
+	db.query("UPDATE member SET ? ?"+"WHERE uid = "+uid, member, function(err, rows){
+		if (err) {
+			ret.brea = false;
+			console.log('update error');
+		}
+		else {
+			ret.brea = true;
+			res.json(ret);
+			console.log('update successfully');
+		}
+	})
+}
+
+//return member's information
+exports.read = function(req, res){
+	var uid = req.query;
+
+	var ret = new Object;
+	ret.uid = uid;
+	ret.object = "member";
+	ret.action = "read";
+
+	if (mid) {
+		db.query("SELECT * FROM member", function(err, rows){
+			if (err) {
+				console.log("db error");
+			}
+			else {
+				res.json(rows);
+				console.log("read successfully");
+			}
+		})
+	}
+	else {
+		db.query("SELECT * FROM member WHERE uid = ?", uid, function(err, rows){
+			if (err) {
+				ret.brea = false;
+				console.log("db error");
+			}
+			else {
+				ret.brea = true;
+
+				console.log("read successfully");
+			} 
+		})
+	}
+}
+//edit member's money
+exports.money = function(req, res){}
+//get emergency
+exports.callhelp = function(req, res){
+	var uid = req.body.uid;
+	var position_e = req.body.position_e;
+	var position_n = req.body.position_n;
+
+	var ret = new Object;
+	ret.uid = uid;
+	ret.object = "member";
+	ret.action = "callhelp";
+	ret.brea = true;
+
+	res.json(ret);
+	console.log("Someone needs help!!!");
+}
