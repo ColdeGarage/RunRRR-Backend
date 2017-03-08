5e2fd0621322ee684d4a5c0507f694e6f103e86d
jwwandy <jwwandy@gmail.com>
2017-03-08 16:29:00 +0000

    Add server starting log

--- src/main.js
+++ src/main.js
@@ -47,6 +47,6 @@ app.get(PREFIX+'/pack/read', pack.read);
 
 
 var server = app.listen(3000, function () {
-    var host = server.address().address;
     var port =  server.address().port;
+    console.log("Start Server at port "+port);
 });
