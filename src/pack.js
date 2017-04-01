2c245ef1b3242a9245e4e703421992b7f63f517b
qmo1222 <howard19192828@gmail.com>
2017-04-01 22:53:28 +0800

    finish pack api

--- src/pack.js
+++ src/pack.js
@@ -3,15 +3,155 @@ var connect = db.conn();
 
 //create a backpack
 exports.create = function(req, res){
-	console.log("This api is still empty.");
+	var pack = new Object;
+	pack.uid = req.body.uid;
+	pack.class = req.body.class;
+	pack.id = req.body.id;
+
+	//check if post all of the values
+	var check = 1;
+	for (var key in pack) {
+		check = check && pack[key];
+	}
+
+	var ret = new Object;
+	ret.uid = req.body.operator_uid;
+	ret.object = "pack";
+	ret.action = "create";
+
+	if (check) {
+		connect.query("INSERT INTO pack SET ?", pack, function(err, rows){
+			if (err){
+				ret.brea = 1;
+				res.json(ret);
+				console.log("db error");
+			}
+			else {
+				ret.brea = 0;
+				console.log("create pack successfully");
+			}
+		});
+		connect.query("SELECT pid FROM pack WHERE uid = "+pack.uid, function(err, rows){
+			if (err){
+				ret.brea = 1;
+				res.json(ret);
+				console.log("db error");
+			}
+			else {
+				if (rows.length == 0) {
+					ret.brea = 1;
+					res.json(ret);
+					console.log("empty db");
+				}
+				else {
+					ret.brea = 0;
+					var info = JSON.parse(rows);
+					ret.payload = {
+						type : "Attribute Name",
+						pid : info.pid
+					}
+					res.json(ret);
+					console.log("get pack id successfully");
+				}
+			}
+		});
+	}
+	else {
+		ret.brea = 2; //if no complete pack values
+		res.json(ret);
+		console.log("uncomplete pack values");
+	}
 }
 
 //delete a backpack
 exports.delete = function(req, res){
-	console.log("This api is still empty.");
+	var pid = req.body.pid;
+
+	var ret = new Object;
+	ret.uid = req.body.operator_uid;
+	ret.object = "pack";
+	ret.action = "delete";
+
+	if (pid) {
+		connect.query("DELETE FROM pack WHERE pid = "+pid, function(err, rows){
+			if (err){
+				ret.brea = 1;
+				res.json(ret);
+				console.log("db error");
+			}
+			else {
+				ret.brea = 0;
+				res.json(ret);
+				console.log("delete pack successfully");
+			}
+		});
+	}
+	else {
+		ret.brea = 2;
+		res.json(ret);
+		console.log("no pid value")
+	}
 }
 
 //read the backpack's information
 exports.read = function(req, res){
-	console.log("This api is still empty.");
+	var pid = req.body.pid;
+
+	var ret = new Object;
+	ret.uid = req.body.operator_uid;
+	ret.object = "pack";
+	ret.action = "read";
+
+	if (pid) {
+		connect.query("SELECT * FROM pack WHERE pid = "+pid, function(err, rows){
+			if (err) {
+				ret.brea = 1;
+				res.json(ret);
+				console.log("db error");
+			}
+			else {
+				if (rows.length == 0) {
+					ret.brea = 1;
+					res.json(ret);
+					console.log("empty db");
+				}
+				else {
+					ret.brea = 0;
+					var info = JSON.parse(rows);
+					ret.payload = {
+						type : "objects",
+						objects : info
+					}
+					res.json(ret);
+					console.log("read successfully");
+				}
+			} 
+		});
+	}
+	else {
+		connect.query("SELECT * FROM pack", function(err, rows){
+			if (err) {
+				ret.brea = 1;
+				res.json(ret);
+				console.log("db error");
+			}
+			else {
+				if (rows.length == 0) {
+					ret.brea = 1;
+					res.json(ret);
+					console.log("empty db");
+				}
+				else {
+					ret.brea = 0;
+					var info = JSON.parse(rows);
+					ret.payload = {
+						type : "objects",
+						objects : info
+					}
+					res.json(ret);
+					console.log("read successfully");
+				}	
+			}
+		});
+	}
 }
