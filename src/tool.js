2a93b4ae5d71ee2be1c46e524391577bcbdfbb5a
qmo1222 <howard19192828@gmail.com>
2017-04-01 22:10:30 +0800

    tool api blocking

--- src/tool.js
+++ src/tool.js
@@ -3,23 +3,23 @@ var connect = db.conn();
 
 //create a new tool
 exports.create = function(req, res){
-	var tool = new Object;
-	tool.title = req.body.title;
-	tool.content = req.body.content;
-	tool.url = req.body.url;
-	tool.expire = req.body.expire;
-	tool.price = req.body.price;
+	// var tool = new Object;
+	// tool.title = req.body.title;
+	// tool.content = req.body.content;
+	// tool.url = req.body.url;
+	// tool.expire = req.body.expire;
+	// tool.price = req.body.price;
 
-	//check if post all of the values
-	var check = 1;
-	for (var key in tool) {
-		check = check && tool[key];
-	}
+	// //check if post all of the values
+	// var check = 1;
+	// for (var key in tool) {
+	// 	check = check && tool[key];
+	// }
 
-	var ret = new Object;
-	ret.uid = req.body.operator_uid;
-	ret.object = "tool";
-	ret.action = "create";
+	// var ret = new Object;
+	// ret.uid = req.body.operator_uid;
+	// ret.object = "tool";
+	// ret.action = "create";
 
 	console.log("This api is still empty.");
 }
