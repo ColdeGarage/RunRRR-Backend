8096ba322693ee25146e0678d7418197136878ed
jwwandy <jwwandy@gmail.com>
2017-04-02 22:53:41 +0000

    add uncomplete request unittest

--- test/test.js
+++ test/test.js
@@ -31,6 +31,19 @@ describe('Member Api', function(){
             done();
         });
     });
+    it('/PUT liveordie(uncomplete request)', function(done) { 
+        chai.request(HOST)
+        .put(path.join(HOST_PREFIX, 'member', 'liveordie'))
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('object').eql('member');
+            res.body.should.have.property('action').eql('liveordie');
+            res.body.should.have.property('brea').eql(2);
+            done();
+        });
+    });
     it('/PUT update', function(done) { // <= Pass in done callback
         var req = {'operator_uid':0, 'uid':12, 'status':1, 'position_e':'120.13', 'position_n':'23.456'};
         chai.request(HOST)
@@ -51,6 +64,20 @@ describe('Member Api', function(){
             done();
         });
     });
+    it('/PUT update(Uncomplete request)', function(done) { // <= Pass in done callback
+        chai.request(HOST)
+        .put(path.join(HOST_PREFIX, 'member', 'update'))
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('object').eql('member');
+            res.body.should.have.property('action').eql('update');
+            res.body.should.have.property('brea').eql(2);
+
+            done();
+        });
+    });
     it('/PUT callhelp', function(done) { // <= Pass in done callback
         var req = {'operator_uid':0, 'uid':12, 'status':1, 'position_e':'120.13', 'position_n':'23.456'};
         chai.request(HOST)
@@ -64,6 +91,20 @@ describe('Member Api', function(){
             res.body.should.have.property('object').eql('member');
             res.body.should.have.property('action').eql('callhelp');
             res.body.should.have.property('brea').to.be.an('number');
+
+            done();
+        });
+    });
+    it('/PUT callhelp(Uncomplete request)', function(done) { // <= Pass in done callback
+        chai.request(HOST)
+        .put(path.join(HOST_PREFIX, 'member', 'callhelp'))
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('object').eql('member');
+            res.body.should.have.property('action').eql('callhelp');
+            res.body.should.have.property('brea').eql(2);
 
             done();
         });
@@ -125,6 +166,20 @@ describe('Member Api', function(){
             done();
         });
     });
+    it('/GET read(Uncomplete request)', function(done) { // <= Pass in done callback
+        chai.request(HOST)
+        .put(path.join(HOST_PREFIX, 'member', 'read'))
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('object').eql('member');
+            res.body.should.have.property('action').eql('read');
+            res.body.should.have.property('brea').eql(2);
+
+            done();
+        });
+    });
     it('/POST login', function(done) { 
         var req = {'operator_uid':0, 'email':'abcd@gmail.com', 'password':'xxxxx'};
         chai.request(HOST)
@@ -145,6 +200,20 @@ describe('Member Api', function(){
             done();
         });
     });
+    it('/POST login(Uncomplete request)', function(done) { // <= Pass in done callback
+        chai.request(HOST)
+        .put(path.join(HOST_PREFIX, 'member', 'login'))
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('object').eql('member');
+            res.body.should.have.property('action').eql('login');
+            res.body.should.have.property('brea').eql(2);
+
+            done();
+        });
+    });
     it('/PUT money', function(done) { 
         var req = {'operator_uid':0, 'uid':12, 'money_amount':-50};
         chai.request(HOST)
@@ -158,6 +227,20 @@ describe('Member Api', function(){
             res.body.should.have.property('object').eql('member');
             res.body.should.have.property('action').eql('money');
             res.body.should.have.property('brea').eql(0);
+
+            done();
+        });
+    });
+    it('/PUT money(Uncomplete request)', function(done) { // <= Pass in done callback
+        chai.request(HOST)
+        .put(path.join(HOST_PREFIX, 'member', 'money'))
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('object').eql('member');
+            res.body.should.have.property('action').eql('money');
+            res.body.should.have.property('brea').eql(2);
 
             done();
         });
