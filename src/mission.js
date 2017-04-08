36d35b39229d460f7b3aa55e8a9d912ba360fc21
qmo <howard19192828@gmail.com>
2017-04-08 12:11:30 +0800

    request class parse

--- src/mission.js
+++ src/mission.js
@@ -10,7 +10,8 @@ exports.create = function(req, res){
 	mission.time_end = req.body.time_end;
 	mission.prize = parseInt(req.body.prize);
 	mission.clue = req.body.clue;
-	mission.class = parseInt(req.body.class);
+	// mission.class = parseInt(req.body.class);
+	mission.class = req.body.class;
 	mission.score = parseInt(req.body.score);
 
 	//check if post all of the values
